a57baafb29a89e26ed5e75a44749a24042a4d05b
Alan Ho <alanhoyu@gmail.com>
2015-06-11 07:56:12 -0700

    calculator commit: reduced space

--- calculator.rb
+++ calculator.rb
@@ -1,18 +1,13 @@
 # Calculator
 puts "Hi, this is your calculator."
-
 loop do
 
 puts "Please enter the first number."  
-
 num1 = gets.chomp
-
 puts "The first number is #{num1}"
 
 puts "Please enter the second number."
-
 num2 = gets.chomp
-
 puts "The second number is #{num2}"
 
 puts "Enter what you wants to do 1) Add 2) Substract 3) Multiply 4) Divide"
@@ -20,7 +15,6 @@ puts "Enter what you wants to do 1) Add 2) Substract 3) Multiply 4) Divide"
 operator = gets.chomp
 
 case operator
-
 when '1'
   answer = num1.to_i + num2.to_i
 when '2'
@@ -34,7 +28,6 @@ else
 end
 
 puts "Answer is #{answer}"
-
 puts "Do you want to continue with another calculation?(Y/N)"
 
 repeat = gets.chomp
